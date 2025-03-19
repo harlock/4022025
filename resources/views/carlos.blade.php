14d05ad4873046ab1da7a20ce1663ce0811b88aa
cdfabilar <carlitoscdfrpollo@gmail.com>
2025-03-19 13:39:48 -0600

    carlos

--- resources/views/carlos.blade.php
+++ resources/views/carlos.blade.php
@@ -1,11 +1,8 @@
 @extends("layouts.app")
 @section("content")
 <div class="row d-flex">
-    <div class="col">
-        <h1 class="alert alert-success">Carlos Daniel Fabila Reyes</h1>
-    </div>
-    <div class="col">
-        <img src="{{asset("IMG/Foto.png")}}" class="img-thumbnail w-25 " alt="...">
+    <div class="col-12">
+        <h1 class="alert alert-success text-center">🌚Carlos Daniel Fabila Reyes🌞</h1>
     </div>
 </div>
 @endsection
