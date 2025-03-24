32ae761f90b1500efc93c39e2739d3d7d8056f7b
Alondra Alvarez <L202307038@vbravo.tecnm.mx>
2025-03-24 09:57:47 -0600

    crud:clasificacion

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::resource('idioma', App\Http\Controllers\IdiomaController::class);
 Route::resource('clasificacion', App\Http\Controllers\ClasificacionController::class);
 
 Route::resource('horas', App\Http\Controllers\HoraController::class);
+
 Route::resource('personas', App\Http\Controllers\PersonasController::class);
 
 Route::get("cesar", function () {
