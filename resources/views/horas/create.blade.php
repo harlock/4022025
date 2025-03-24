6f1d9d1444fb9984d947f9d155542011048386de
Imanol <imanol10albiter@gmail.com>
2025-03-24 10:54:15 -0600

    Validacion horas

--- resources/views/horas/create.blade.php
+++ resources/views/horas/create.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+   
     <div class="row justify-content-center">
         <div class="col-8">
             <h1 class="alert alert-success">Agregar hroas</h1>
             <a href="{{route('horas.index')}}" class="btn btn-primary">Regresar</a>
         </div>
     </div>
+    @if ($errors->any())
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    @endif
 
     <div class="row justify-content-center mt-5">
         <div class="col-6">
@@ -14,7 +28,7 @@
                 @csrf
                 <div class="mb-3">
                     <label for="descripcion_h" class="form-label">Descripción de horas</label>
-                    <input type="text" class="form-control" id="descripcion_h" name="descripcion_h" required>
+                    <input type="text" class="form-control" id="descripcion_h" name="descripcion_h">
                 </div>
 
                 <button type="submit" class="btn btn-primary">Guardar</button>
