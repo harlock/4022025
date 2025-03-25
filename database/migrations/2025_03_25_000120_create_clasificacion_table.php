e1bddc7906ea57a30484a218a9a9159dba6050b4
Alondra Alvarez <L202307038@vbravo.tecnm.mx>
2025-03-24 18:06:06 -0600

    Agrega migración para la tabla clasificacion

---
+++ database/migrations/2025_03_25_000120_create_clasificacion_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('clasificacion', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_clasifi'); // Define la columna correctamente
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('clasificacion');
+    }
+};
+
