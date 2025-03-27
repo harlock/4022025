d0fb8496c2039e9197cff1311f19e175f8db3491
Axolotl GG <emigm144@gmail.com>
2025-03-27 10:41:38 -0600

    migracion horas

--- resources/views/horas/edit.blade.php
+++ resources/views/horas/edit.blade.php
@@ -5,20 +5,19 @@
         <div class="col-8">
             <h1 class="alert alert-success">Editar horas</h1>
             <a href="{{route('horas.index')}}" class="btn btn-primary">Regresar</a>
-        </div>
-    </div>
+        </div>   
+    </div>    
     @if ($errors->any())
         <div class="row justify-content-center">
             <div class="col-4">
                 <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
+                    <ul>                        @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
                         @endforeach
                     </ul>
-                </div>
-            </div>
-        </div>
+                </div>          
+            </div>     
+        </div>    
     @endif
     <div class="row justify-content-center mt-5">
         <div class="col-6">
@@ -29,9 +28,8 @@
                     <label for="descripcion_h" class="form-label">Descripción de horas</label>
                     <input type="text" class="form-control" id="descripcion_h" name="descripcion_h" value="{{ $hora->descripcion_h }}" >
                 </div>
-
                 <button type="submit" class="btn btn-primary">Actualizar</button>
-            </form>
-        </div>
+            </form>        
+        </div>    
     </div>
 @endsection
