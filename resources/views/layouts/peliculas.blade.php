52b3ddeffa332fb0349a01f2febc183582fc0eab
Jose Arturo <artuxcarbajal@gmail.com>
2025-03-30 11:05:01 -0600

    edicion de sidebar peliculas

--- resources/views/layouts/peliculas.blade.php
+++ resources/views/layouts/peliculas.blade.php
@@ -21,29 +21,34 @@
 <main class="container-fluid bg-light">
     <div class="row min-vh-100">
         <div class="col-2 d-flex flex-column align-items-center bg-dark text-white p-3">
-            <img src="{{asset('img/perfil.jpeg')}}" class="rounded-circle w-50 mb-3" alt="logo">
+            <img src="{{asset('img/perfil.png')}}" class="rounded-circle w-50 mb-3" alt="logo">
 
             <h4 class="fw-bold text-center"></h4>
+            
             <ul class="nav flex-column w-100 mt-3">
-                <a href="#" class="text-decoration-none fw-bold text-white"><li class="nav-item py-2"><i class="fa-solid fa-chart-line me-2"></i> Analisis</li></a>
-                <a href="#" class="text-decoration-none fw-bold text-white"<li class="nav-item py-2"><i class="fa-solid fa-users me-2"></i> Usuarios</li></a>
-                <a href="#" class="text-decoration-none fw-bold text-white"><li class="nav-item py-2"><i class="fa-solid fa-server me-2"></i> Servidores</li></a>
-                <a href="#" class="text-decoration-none fw-bold text-white"><li class="nav-item py-2"><i class="fa-solid fa-cloud me-2"></i> Almacenaje en la Nube</li></a>
-                <a href="#" class="text-decoration-none fw-bold text-white"><li class="nav-item py-2"><i class="fa-solid fa-cogs me-2"></i> Configuracion</li></a>
+                <li class="nav-item py-2 {{ Request::is('dashPeliculas') ? 'bg-primary rounded' : '' }}"><a href="{{ asset('dashPeliculas') }}" class="text-decoration-none fw-bold text-white  align-items-center gap-4 d-flex"><i class="fa-solid fa-film ms-4"></i>Peliculas</a></li>
+                <li class="nav-item py-2 {{ Request::is('clasificacion*') ? 'bg-primary rounded' : '' }}"><a href="{{ route('clasificacion.index') }}" class="text-decoration-none fw-bold text-white align-items-center gap-4 d-flex"><i class="fa-solid fa-clapperboard ms-4"></i>Clasificacion</a></li>
+                <li class="nav-item py-2 {{ Request::is('genero*') ? 'bg-primary rounded' : '' }}"><a href="{{ route('generos.index') }}" class="text-decoration-none fw-bold text-white align-items-center gap-4 d-flex"><i class="fa-solid fa-users ms-4"></i> Generos</a></li>
+                <li class="nav-item py-2 {{ Request::is('idioma*') ? 'bg-primary rounded' : '' }}"><a href="{{ route('idioma.index') }}" class="text-decoration-none fw-bold text-white align-items-center gap-4 d-flex"><i class="fa-solid fa-language ms-4"></i> Idiomas</a></li>
+                <li class="nav-item py-2 {{ Request::is('director*') ? 'bg-primary rounded' : '' }}"><a href="#" class="text-decoration-none fw-bold text-white align-items-center gap-4 d-flex"><i class="fa-solid fa-cloud ms-4"></i>Directores</a></li>
             </ul>
+
             <div class="mt-auto">
-                <button class="btn btn-azul w-100"><i class="fa-solid fa-sign-out-alt"></i> Logout</button>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn w-100 text-white">
+                        <i class="fa-solid fa-sign-out-alt"></i> Logout
+                    </button>
+                </form>
             </div>
+            
         </div>
+        
         <div class="col pt-4">
             @yield('content')
         </div>
         
     </div>
 </main>
-
-<footer class="bg-dark text-white text-center py-3">
-    <p>Todos los derechos reservados</p>
-</footer>
 </body>
 </html>
