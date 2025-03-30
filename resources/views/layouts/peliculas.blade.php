d4b76cf21cbdd1611b0f5859b19f4e4d655534d2
unknown <L202307049@vbravo.tecnm.mx>
2025-03-30 13:39:06 -0600

    correccion-dash

--- resources/views/layouts/peliculas.blade.php
+++ resources/views/layouts/peliculas.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="es">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,55 +16,15 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-
 <body>
 
-<<<<<<< HEAD
-    <main class="container-fluid bg-light">
-        <div class="row min-vh-100">
-            <div class="col-2 d-flex flex-column align-items-center bg-dark text-white p-3">
-                <img src="{{asset('img/perfil.jpeg')}}" class="rounded-circle w-50 mb-3" alt="logo">
-
-                <h4 class="fw-bold text-center"></h4>
-                <ul class="nav flex-column w-100 mt-3">
-                    <a href="#" class="text-decoration-none fw-bold text-white">
-                        <li class="nav-item py-2"><i class="fa-solid fa-chart-line me-2"></i> Analisis</li>
-                    </a>
-                    <a href="#" class="text-decoration-none fw-bold text-white">
-                        <li class="nav-item py-2"><i class="fa-solid fa-users me-2"></i> Usuarios</li>
-                    </a>
-                    <a href="#" class="text-decoration-none fw-bold text-white">
-                        <li class="nav-item py-2"><i class="fa-solid fa-server me-2"></i> Servidores</li>
-                    </a>
-                    <a href="#" class="text-decoration-none fw-bold text-white">
-                        <li class="nav-item py-2"><i class="fa-solid fa-cloud me-2"></i> Almacenaje en la Nube</li>
-                    </a>
-                    <a href="#" class="text-decoration-none fw-bold text-white">
-                        <li class="nav-item py-2"><i class="fa-solid fa-cogs me-2"></i> Configuracion</li>
-                    </a>
-                </ul>
-                <div class="mt-auto">
-                    <button class="btn btn-azul w-100"><i class="fa-solid fa-sign-out-alt"></i> Logout</button>
-                </div>
-            </div>
-            <div class="col pt-4">
-                @yield('content')
-            </div>
-
-        </div>
-    </main>
-
-    <footer class="bg-dark text-white text-center py-3">
-        <p>Todos los derechos reservados</p>
-    </footer>
-=======
 <main class="container-fluid bg-light">
     <div class="row min-vh-100">
         <div class="col-2 d-flex flex-column align-items-center bg-dark text-white p-3">
             <img src="{{asset('img/perfil.png')}}" class="rounded-circle w-50 mb-3" alt="logo">
 
             <h4 class="fw-bold text-center"></h4>
-            
+
             <ul class="nav flex-column w-100 mt-3">
                 <li class="nav-item py-2 {{ Request::is('dashPeliculas') ? 'bg-primary rounded' : '' }}"><a href="{{ asset('dashPeliculas') }}" class="text-decoration-none fw-bold text-white  align-items-center gap-4 d-flex"><i class="fa-solid fa-film ms-4"></i>Peliculas</a></li>
                 <li class="nav-item py-2 {{ Request::is('clasificacion*') ? 'bg-primary rounded' : '' }}"><a href="{{ route('clasificacion.index') }}" class="text-decoration-none fw-bold text-white align-items-center gap-4 d-flex"><i class="fa-solid fa-clapperboard ms-4"></i>Clasificacion</a></li>
@@ -82,16 +41,14 @@
                     </button>
                 </form>
             </div>
-            
+
         </div>
-        
+
         <div class="col pt-4">
             @yield('content')
         </div>
-        
+
     </div>
 </main>
->>>>>>> e8dae021d94ed0fb75ad7293b4159176ad0715f1
 </body>
-
 </html>
