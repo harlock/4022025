fe968e14e785d613adbe2974077c979015762391
Edu6178 <l202307053@vbravo.tecnm.mx>
2025-03-31 06:51:58 -0600

    checando pero sin cambios

--- routes/web.php
+++ routes/web.php
@@ -139,11 +139,11 @@ Route::get("charbel", function () {
     return view('charbel');
 });
 
-<<<<<<< HEAD
+
 Route::get('cines', function () {
     return view('cines');
 });
-=======
+
 Route::get("dashPeliculas", function () {
    return view('peliculasViews/dashPeliculas');
 });
@@ -153,4 +153,3 @@ Route::get("dashAsignaC", function () {
  });
 
 
->>>>>>> afb8947c57bbc4e67d5ff29143aa78aeb6dd3ce7
